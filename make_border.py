4534d5cc77ff360a1330d0d79092123305d112fd
Sim Gyeongbo <gbsim@laonpeople.com>
2020-07-02 10:26:48 +0900

    add augmentation

---
+++ make_border.py
@@ -0,0 +1,82 @@
+'''
+* convert2YOLO
+* Version: 1.0
+* Author: gbsim (asdf2kr@naver.com)
+* Since 2020-06-30
+'''
+import os
+import cv2
+import argparse
+from util import printProgress
+def main():
+    ''' Main function'''
+    parser = argparse.ArgumentParser(description='Convert from datasets (COCO, COCO_YOLO, UA-DETRAC, MOT2020, Bdd100k, AIHub, CrowdHuman) to YOLO format.')
+    parser.add_argument('--imgDir', type=str, help='directory of images.', default='./copy')
+    parser.add_argument('--saveDir', type=str, help='directory of images.', default='./augmentation')
+
+    config = parser.parse_args()
+
+    files = os.listdir(config.imgDir)
+
+    if not os.path.exists(config.saveDir):
+        os.makedirs(config.saveDir)
+
+    progressCnt = 0
+    printProgress(progressCnt, len(files), ' Data-augmentation Progress: ', 'Complete')
+
+    BLACK = [0, 0, 0]
+    for file in files:
+        fileName, fextension = os.path.splitext(file)
+        if fextension == '.jpg' or fextension =='.png':
+            print("[info] file ", file)
+            img = cv2.imread(os.path.join(config.imgDir, file), cv2.IMREAD_COLOR)
+            height = float(img.shape[0])
+            width = float(img.shape[1])
+
+            ratio = 1
+            dx = int(width * ratio / 2.0)
+            dy = int(height * ratio / 2.0)
+            dst = cv2.copyMakeBorder(img, top=dy, bottom=dy, left=dx, right=dx, borderType=cv2.BORDER_CONSTANT, value=BLACK)
+
+            lines = ''
+            writeStr = ''
+            writeStr2 = ''
+
+            txtFile = fileName + '.txt'
+            with open(os.path.join(config.imgDir, txtFile), 'r+') as f:
+                lines = f.readlines()
+
+
+            for j in range(len(lines)):
+                label, cx, cy, w, h = lines[j].split(' ')[0:5]
+                cx, cy, w, h = float(cx), float(cy), float(w), float(h)
+
+                if min(w, h) > 0.015 or max(w, h) > 0.03:
+                    writeStr += "{} {} {} {} {}\n".format(label, cx, cy, w, h)
+
+                labelWidth = width * w
+                labelHeight = height * h
+                labelCx = cx * w
+                labelCy = cy * h
+
+                newCx = (dx + labelCx) / (width + (width * ratio))
+                newCy = (dy + labelCy) / (height + (height * ratio))
+                newW =  (w / (1 + ratio))
+                newH =  (h / (1 + ratio))
+
+                if min(newW, newH) > 0.015 or max(newW, newH) > 0.03:
+                    writeStr2 += "{} {} {} {} {}\n".format(label, newCx, newCy, newW, newH)
+
+            if writeStr:
+                with open(os.path.join(config.saveDir, txtFile), 'w+') as f:
+                    f.write(writeStr)
+            if writeStr2:
+                with open(os.path.join(config.saveDir, fileName + '-border.txt'), 'w+') as f:
+                    f.write(writeStr2)
+                cv2.imwrite(os.path.join(config.saveDir, fileName + '-border' + fextension), dst)
+            progressCnt += 1
+            printProgress(progressCnt, len(files), ' Data-augmentation Progress: ', 'Complete')
+
+
+if __name__ == '__main__':
+    main()
--- show.py
+++ show.py
@@ -5,54 +5,61 @@
 * Since 2020-06-30
 '''
 import cv2
+import os
 import random
 import argparse
 def main():
     ''' Main function'''
+    parser = argparse.ArgumentParser(description='Convert from datasets (COCO, COCO_YOLO, UA-DETRAC, MOT2020, Bdd100k, AIHub, CrowdHuman) to YOLO format.')
     parser.add_argument('--imgDir', type=str, help='directory of images.')
+    parser.add_argument('--saveDir', type=str, help='directory of images.')
     # parser.add_argument('--img', type=str, help='select the target image.')
     parser.add_argument('--random', type=int, help='', default=0)
     config = parser.parse_args()
-    config.imgDir="E:\\DB\\coco_yolo\\images_ped_car_type\\train"
-    config.random = 3
-    if config.random > 0:
-        file = os.listdir(config.imageDir)
-        for i in range(config.random):
-            rand = random.randrange(0, len(file))
+    config.imgDir="E:\\DB\\copy\\aihub"
+    config.saveDir="E:\\DB\\copy\\save"
+    config.random = 5
 
+    if not os.path.exists(config.saveDir):
+        os.makedirs(config.saveDir)
+
+    if config.random > 0:
+        file = os.listdir(config.imgDir)
+        while(config.random):
+            rand = random.randrange(0, len(file))
             if file[rand].split('.')[-1] == 'txt':
-                i -= 1
                 continue
 
             imgFile =file[rand]
-            print("[Info] select the {}".format(imgFile))
+            print("[Info] select the {}".format(os.path.join(config.imgDir, imgFile)))
 
-            img = cv.imread(imgFile, cv2.IMREAD_COLOR)
-            width = img.shape[0]
-            height = img.shape[1]
-
+            img = cv2.imread(os.path.join(config.imgDir, imgFile), cv2.IMREAD_COLOR)
+            height = float(img.shape[0])
+            width = float(img.shape[1])
             lines = ''
 
-            with open(file[rand].split('.')[:-1] + '.txt') as f:
+            with open(os.path.join(config.imgDir, ''.join(file[rand].split('.')[:-1]) + '.txt')) as f:
                 lines = f.readlines()
 
             for j in range(len(lines)):
-                label, cx, cy, w, h = lines[j].split(' ')
+
+                label, cx, cy, w, h = lines[j].split(' ')[0:5]
+                w, h, cx, cy = float(w), float(h), float(cx), float(cy)
+
                 w = width * w
                 h = height * h
-                cx = cx * w
-                cy = cy * h
+                cx = width * cx
+                cy = height * cy
 
-                tlx = cx - (w / 2)
-                tly = cy - (h / 2)
-                brx = cx + (w / 2)
-                bry = cy + (h / 2)
+                tlx = int(cx - (w / 2))
+                tly = int(cy - (h / 2))
+                brx = int(cx + (w / 2))
+                bry = int(cy + (h / 2))
+
                 img = cv2.rectangle(img, (tlx, tly), (brx, bry), (0, 255, 0), 3)
 
-            cv2.imshow('image-' + i, img)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-
+            cv2.imwrite(os.path.join(config.saveDir, imgFile), img)
+            config.random -= 1
 
 if __name__ == '__main__':
     main()
